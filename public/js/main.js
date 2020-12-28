f32842854b63c2d4deb8cc8e7005880812679dc7
Jamila <jaja_mimi@hotmail.com>
2020-12-28 15:50:11 +0100

    exo4

---
+++ public/js/main.js
@@ -0,0 +1,29 @@
+// exercice 1 
+
+
+// let nom = 'hammani' // a
+
+// let prenom = ' jamila' // b
+
+// let ville = prompt('de quelle ville tu viens?'); // c
+
+// console.log(`${nom + prenom}`); // d
+// alert(ville)
+
+// exo 2
+
+// a
+
+// let nom = 'Hammani'
+// let prenom ="jamila"
+// let villeDepart = "Bruxelles"
+// let villeDestination = "Tanger"
+// console.log(`Le voyage de ${villeDepart} vers ${villeDestination} a été enregistré au nom de ${nom} ${prenom}`);
+
+
+// b
+let nom = prompt('quel est votre nom?');
+let prenom = prompt('quel est votre prenom?');
+let villeDepart = prompt('de quelle ville partez vous?');
+let villeDestination = prompt('quel est votre destination?');
+console.log(`Le voyage de ${villeDepart} vers ${villeDestination} a été enregistré au nom de ${nom} ${prenom}`);
